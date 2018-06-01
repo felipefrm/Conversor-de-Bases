f5fe754a67e571b3a59c893bb4d3df1f84580b29
Felipe Melo <36086450+shadowffrm@users.noreply.github.com>
2018-06-01 11:31:47 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,339 +1,104 @@
-  #include <stdio.h>
-  #include <stdlib.h>
-  #include <string.h>
-  #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "convercoes.h"
 
-  #define N 51
+#define N 51
 
-  int transforma_char(char num);
+int main(void){
 
-  char transforma_int(int num);
+  char num[N], num_convert[N];                         // Variavies necessárias
+  int b_origem, b_destino, num_digitos, num_char[N], i, k;
+  int ponto, tem_sinal, verif_origem=1, verif_destino=1, tem_valor, verif_num; //flags
+  float fracionaria, n;
+  unsigned long long int inteira;
 
-  int verifica_valor(char num[], int num_digitos);
+  while (1){        // loop infinito para inserir as entradas e repeti-las caso
+                   // não estejam de acordo.
+                  // verificação feita por meio de funções com o auxilio de flags.
 
-  int verifica_sinal(char num[]);
+    setbuf(stdin, NULL);
+    scanf("%s%d%d", num, &b_origem, &b_destino);
+    num_digitos = strlen(num);
 
-  int verifica_ponto(char num[], int num_digitos);
+    tem_valor = verifica_valor(num, num_digitos);
+    if (num[0] == '+' && tem_valor == 0 && b_origem == 0 && b_destino == 0)
+      break;
 
-  int verifica_numero(char num[], int b_origem, int ponto, int num_digitos);
+    if (num_digitos > 50)
+      fprintf(stderr, "Insira um valor com menos de 50 dígitos...\n");
 
-  int verifica_origem(int b_origem);
+    tem_sinal = verifica_sinal(num);
+    if (tem_sinal == 0){
+      fprintf(stderr, "Insira o sinal antes do valor...\n");
+    }
 
-  int verifica_destino(int b_destino);
+    ponto = verifica_ponto(num, num_digitos);
+    if (ponto == 0){
+      fprintf(stderr, "Insira o '.' separando a parte inteira da fracão...\n");
+    }
 
-  int inteiro_para_baseX(unsigned long long int *inteira, int b_destino);
+    verif_num = verifica_numero(num, b_origem, ponto, num_digitos);
+    if (verif_num == 0)
+      fprintf(stderr, "Este valor não pertence a base %d...\n", b_origem);
 
-  int fracao_para_baseX(double *fracionaria, int b_destino);
+    verif_origem = verifica_origem(b_origem);
+    if (verif_origem == 0){
+      fprintf(stderr, "Digite novamente a base de origem...\n");
+    }
 
-  int recupera_inteiro(double n);
+    verif_destino = verifica_destino(b_destino);
+    if (verif_destino == 0){
+      fprintf(stderr, "Digite novamente a base de destino...\n");
+    }
 
-  double recupera_fracao(double n);
+    if(tem_sinal == 1 && verif_origem == 1 && verif_destino == 1){
+      if (tem_valor == 1 && ponto != 0 && verif_num == 1 && num_digitos <= 50){
 
-  int inteiro_para_base10(char num[], int b_origem, int ponto, int num_char[]);
+        for (i=1; i<num_digitos; i++)
+          num_char[i] = transforma_char(num[i]);
 
-  double fracao_para_base10(char num[], int b_origem, int ponto, int digitos, int num_char[]);
+        inteira = inteiro_para_base10(num, b_origem, ponto, num_char);
+        fracionaria = fracao_para_base10(num, b_origem, ponto, num_digitos, num_char);
 
+        if (b_destino == 10){
 
-  int main(void){
+          if (fracionaria == 0)
+            printf("%c%llu.\n", num[0], inteira);
+          else
+            printf("%c%g\n", num[0], inteira + fracionaria);
+        }          //"%g" para remover os zeros a direita da fração.
 
-    char num[N], num_convert[N];                         // Variavies necessárias
-    int b_origem, b_destino, num_digitos, num_char[N], i, k;
-    int ponto, tem_sinal, verif_origem=1, verif_destino=1, tem_valor, verif_num; //flags
-    double fracionaria, n;
-    unsigned long long int inteira;
+        else {
 
-    while (1){        // loop infinito para inserir as entradas e repeti-las caso
-                     // não estejam de acordo.
-                    // verificação feita por meio de funções com o auxilio de flags.
-
-      setbuf(stdin, NULL);
-      scanf("%50s%d%d", num, &b_origem, &b_destino);
-      num_digitos = strlen(num);
-
-      tem_valor = verifica_valor(num, num_digitos);
-      if (num[0] == '+' && tem_valor == 0 && b_origem == 0 && b_destino == 0)
-        break;
-
-      tem_sinal = verifica_sinal(num);
-      if (tem_sinal == 0){
-        fprintf(stderr, "Insira o sinal antes do valor...\n");
-      }
-
-      ponto = verifica_ponto(num, num_digitos);
-      if (ponto == 0){
-        fprintf(stderr, "Insira o '.' separando a parte inteira da fracão...\n");
-      }
-
-      verif_num = verifica_numero(num, b_origem, ponto, num_digitos);
-      if (verif_num == 0)
-        fprintf(stderr, "Este valor não pertence a base %d...\n", b_origem);
-
-      verif_origem = verifica_origem(b_origem);
-      if (verif_origem == 0){
-        fprintf(stderr, "Digite novamente a base de origem...\n");
-      }
-
-      verif_destino = verifica_destino(b_destino);
-      if (verif_destino == 0){
-        fprintf(stderr, "Digite novamente a base de destino...\n");
-      }
-
-      if(tem_sinal == 1 && verif_origem == 1 && verif_destino == 1){
-        if (tem_valor == 1 && ponto != 0 && verif_num == 1){
-
-          for (i=1; i<num_digitos; i++)
-            num_char[i] = transforma_char(num[i]);
-
-          inteira = inteiro_para_base10(num, b_origem, ponto, num_char);
-          fracionaria = fracao_para_base10(num, b_origem, ponto, num_digitos, num_char);
-
-          if (b_destino == 10){
-
-            if (fracionaria == 0)
-              printf("%c%llu.\n", num[0], inteira);
-            else
-              printf("%c%g\n", num[0], inteira + fracionaria);
-          }          //"%g" para remover os zeros a direita da fração.
-
-          else {
-
-            for (i = ponto -1;; i--){
-              num_convert[i] = inteiro_para_baseX(&inteira, b_destino);
-              if (inteira == 0)
-                break;
-            }
-            k = i;    // guarda o valor de i, pois precisará dele futuramente.
-
-            num_convert[ponto] = '.';
-            for (i = ponto + 1;; i++){
-              num_convert[i] = fracao_para_baseX(&fracionaria, b_destino);
-              if (fracionaria == 0)
-                break;
-            }
-
-            while (1){
-              if (num_convert[i] != 0)
-                break;
-              i--;          // guarda a posição em que termina os 0's a direita
-            }
-
-            printf("%c", num[0]);   //imprime o sinal
-            for (; k <= i; k++){
-              printf("%c", transforma_int(num_convert[k]));
-            }
-            printf("\n");
+          for (i = ponto -1;; i--){
+            num_convert[i] = inteiro_para_baseX(&inteira, b_destino);
+            if (inteira == 0)
+              break;
           }
+          k = i;    // guarda o valor de i, pois precisará dele futuramente.
+
+          num_convert[ponto] = '.';
+          for (i = ponto + 1;; i++){
+            num_convert[i] = fracao_para_baseX(&fracionaria, b_destino);
+            if (fracionaria == 0)
+              break;
+          }
+
+          while (1){
+            if (num_convert[i] != 0)
+              break;
+            i--;          // guarda a posição em que termina os 0's a direita
+          }
+
+          printf("%c", num[0]);   //imprime o sinal
+          for (; k <= i; k++){
+            printf("%c", transforma_int(num_convert[k]));
+          }
+          printf("\n");
         }
       }
     }
   }
-
-  int transforma_char(char num){        // FUNÇÕES
-
-    if (num >= 48 && num <= 57)
-      num = num - 48;
-
-    switch(num){
-      case 'A':
-        num = 10;
-        break;
-      case 'B':
-        num = 11;
-        break;
-      case 'C':
-        num = 12;
-        break;
-      case 'D':
-        num = 13;
-        break;
-      case 'E':
-        num = 14;
-        break;
-      case 'F':
-        num = 15;
-        break;
-    }
-    return num;
-  }
-
-  char transforma_int(int num){
-
-    if (num >= 0 && num <= 9)
-      num = num + 48;
-
-    switch(num){
-      case 10:
-        num = 'A';
-        break;
-      case 11:
-        num = 'B';
-        break;
-      case 12:
-        num = 'C';
-        break;
-      case 13:
-        num = 'D';
-        break;
-      case 14:
-        num = 'E';
-        break;
-      case 15:
-        num = 'F';
-        break;
-    }
-    return num;
-  }
-
-  int verifica_valor(char num[], int num_digitos){
-    int tem_valor = 0;
-    for (int i=1; i<num_digitos; i++){    //i=0 é o sinal.
-      if(num[i] != '0'){
-        tem_valor = 1;
-      }
-    }
-    return tem_valor;
-  }
-
-  int verifica_sinal(char num[]){
-    int tem_sinal = 1;
-    if (num[0] != '+' && num[0] != '-'){
-      tem_sinal = 0;
-    }
-    return tem_sinal;
-  }
-
-  int verifica_ponto(char num[], int num_digitos){
-    int ponto = 0;
-    for (int i=1; i < num_digitos; i++){
-      if (num[i] == '.'){
-        ponto = i;
-        return ponto;
-      }
-    }
-    return ponto;
-  }
-
-  int verifica_numero(char num[], int b_origem, int ponto, int num_digitos){
-
-    int i, verif_num = 1;
-    if (b_origem == 2){
-      for (i=1; i<ponto; i++){
-        if (num[i] < 48 || num[i] > 49){
-          verif_num = 0;
-          return verif_num;
-        }
-      }
-      for (i=ponto+1; i<num_digitos; i++){
-        if (num[i] < 48 || num[i] > 49){
-          verif_num = 0;
-          return verif_num;
-        }
-      }
-    }
-    else if (b_origem == 8){
-      for (i=1; i<ponto; i++){
-        if (num[i] < 48 || num[i] > 55){
-          verif_num = 0;
-          return verif_num;
-        }
-      }
-      for (i=ponto+1; i<num_digitos; i++){
-        if (num[i] < 48 || num[i] > 55){
-          verif_num = 0;
-          return verif_num;
-        }
-      }
-    }
-    else if (b_origem == 10){
-      for (i=1; i<ponto; i++){
-        if (num[i] < 48 || num[i] > 57){
-          verif_num = 0;
-          return verif_num;
-        }
-      }
-      for (i=ponto+1; i<num_digitos; i++){
-        if (num[i] < 48 || num[i] > 57){
-          verif_num = 0;
-          return verif_num;
-        }
-      }
-    }
-    else if (b_origem == 16){
-      for (i=1; i<ponto; i++){
-        if ((num[i] < 48 || num[i] > 57) && (num[i] < 'A' || num[i] > 'F')){
-          verif_num = 0;
-          return verif_num;
-        }
-      }
-      for (i=ponto+1; i<num_digitos; i++){
-        if ((num[i] < 48 || num[i] > 57) && (num[i] < 'A' || num[i] > 'F')){
-          verif_num = 0;
-          return verif_num;
-        }
-      }
-    }
-    return verif_num;
-  }
-
-  int verifica_origem(int b_origem){
-    int verif_origem = 1;
-    if (b_origem != 2 && b_origem != 8 && b_origem != 10 && b_origem != 16){
-      verif_origem = 0;
-    }
-    return verif_origem;
-  }
-
-  int verifica_destino(int b_destino){
-    int verif_destino = 1;
-    if (b_destino != 2 && b_destino != 8 && b_destino != 10 && b_destino != 16){
-      verif_destino = 0;
-    }
-    return verif_destino;
-  }
-
-  int recupera_inteiro(double n){
-      int num = (int)n;
-      return num;
-  }
-
-  double recupera_fracao(double n){
-      double num;
-      num = n - (int)n;
-      return num;
-  }
-
-  int inteiro_para_baseX(unsigned long long int *inteira, int b_destino){
-    int n;
-    n = *inteira % b_destino;
-    *inteira = *inteira / b_destino;
-    return n;
-  }
-
-  int fracao_para_baseX(double *fracionaria, int b_destino){
-    int n;
-    double p;
-    p = *fracionaria * b_destino;
-    n = recupera_inteiro(p);
-    *fracionaria = recupera_fracao(p);
-    return n;
-  }
-
-  int inteiro_para_base10(char num[], int b_origem, int ponto, int num_char[]){
-    int i, expoente = ponto -2;  //desconsidera o PONTO e o SINAL.
-    unsigned long long soma = 0;
-    for (i=1; i < ponto; i++){
-      soma += num_char[i] * pow(b_origem, expoente);
-      expoente--;
-    }
-    return soma;
-  }
-
-  double fracao_para_base10(char num[], int b_origem, int ponto, int digitos, int num_char[]){
-    int i, expoente = -1;
-    double soma=0;
-    for (i = ponto+1; i < digitos; i++){
-      soma += num_char[i] * pow(b_origem, expoente);
-      expoente --;
-    }
-    return soma;
-  }
+}
