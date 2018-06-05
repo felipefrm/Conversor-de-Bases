9a515e6a887c0177ea0c010fa7b2db86c9eefb28
Felipe Melo <36086450+shadowffrm@users.noreply.github.com>
2018-06-05 13:42:39 -0300

    Update convercoes.h

--- convercoes.h
+++ convercoes.h
@@ -1,19 +1,13 @@
 #ifndef CONVERSAO
 #define CONVERSAO
 
-int transforma_char(char num);
-char transforma_int(int num);
-int verifica_valor(char num[], int num_digitos);
-int verifica_sinal(char num[]);
-int verifica_ponto(char num[], int num_digitos);
-int verifica_numero(char num[], int b_origem, int ponto, int num_digitos);
-int verifica_origem(int b_origem);
-int verifica_destino(int b_destino);
-int inteiro_para_baseX(unsigned long long int *inteira, int b_destino);
-int fracao_para_baseX(float *fracionaria, int b_destino);
-int recupera_inteiro(float n);
-float recupera_fracao(float n);
-unsigned long long int inteiro_para_base10(char num[], int b_origem, int ponto, int num_char[]);
-float fracao_para_base10(char num[], int b_origem, int ponto, int digitos, int num_char[]);
+int transformaChar(char num);
+char transformaInt(int num);
+int inteiroParaBaseX(unsigned long long int *inteira, int b_destino);
+int fracaoParaBaseX(double *fracionaria, int b_destino);
+int recuperaInteiro(double n);
+double recupera_fracao(double n);
+unsigned long long int inteiroParaBase10(char num[], int b_origem, int ponto, int num_char[]);
+double fracaoParaBase10(char num[], int b_origem, int ponto, int digitos, int num_char[]);
 
 #endif
