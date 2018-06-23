cc56540c052cf797c553de8eeccafe3ea1d66ea2
Felipe Melo <36086450+shadowffrm@users.noreply.github.com>
2018-06-22 21:33:34 -0300

    Update verificacoes.h

--- verificacoes.h
+++ verificacoes.h
@@ -1,10 +1,10 @@
 #ifndef VERIFICACAO
 #define VERIFICACAO
 
-int verificaValor(char num[], int num_digitos);
-int verificaSinal(char num[]);
-int verificaPonto(char num[], int num_digitos);
-int verificaNumero(char num[], int b_origem, int ponto, int num_digitos);
+int verificaValor(char *num, int num_digitos);
+int verificaSinal(char *num);
+int verificaPonto(char *num, int num_digitos);
+int verificaNumero(char *num, char *numX, int j, int b_origem, int ponto, int num_digitos);
 int verificaOrigem(int b_origem);
 int verificaDestino(int b_destino);
 
