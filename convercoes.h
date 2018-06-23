a741ba1ad10cd99d0407a3214c688b219cde6615
Felipe Melo <36086450+shadowffrm@users.noreply.github.com>
2018-06-22 21:32:30 -0300

    Update convercoes.h

--- convercoes.h
+++ convercoes.h
@@ -1,13 +1,17 @@
 #ifndef CONVERSAO
 #define CONVERSAO
 
+int transformaXchar(char c, char *num, int k);
+char transformaXint(char num, int j, char *numX);
 int transformaChar(char num);
 char transformaInt(int num);
-int inteiroParaBaseX(unsigned long long int *inteira, int b_destino);
-int fracaoParaBaseX(double *fracionaria, int b_destino);
+unsigned long long int intBaseXpara10(int k, int baseX, int ponto, char *num, char *str);
+double fracBaseXpara10(int k, int baseX, int ponto, char *num, char *str);
+int inteiroParaDestino(unsigned long long int *inteira, int b_destino);
+int fracaoParaDestino(double *fracionaria, int b_destino);
 int recuperaInteiro(double n);
-double recupera_fracao(double n);
-unsigned long long int inteiroParaBase10(char num[], int b_origem, int ponto, int num_char[]);
-double fracaoParaBase10(char num[], int b_origem, int ponto, int digitos, int num_char[]);
+double recuperaFracao(double n);
+unsigned long long int inteiroParaBase10(char *num, int b_origem, int ponto, int *num_char);
+double fracaoParaBase10(char *num, int b_origem, int ponto, int digitos, int *num_char);
 
 #endif
